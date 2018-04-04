dac08142b084ba3b62e74f9a40cdfadce3748fa5
XenonCoder <xenon77.dev@gmail.com>
2018-04-05 01:46:19 +0200

    #13 Adding the map save/load system

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ from pyglet.gl import *
 from pyglet.graphics import TextureGroup
 from pyglet.window import key, mouse
 
+import saveModule
+
 
 ############################## ENGINE SETTINGS ##############################
 
@@ -231,44 +233,51 @@ class Model(object):
         # _show_block() and _hide_block() calls
         self.queue = deque()
 
+        # A module to save and load the world
+        self.saveModule = saveModule.saveModule()
+
         self._initialize()
 
     def _initialize(self):
         """ Initialize the world by placing all the blocks.
 
         """
-        n = 80  # 1/2 width and height of world
-        s = 1  # step size
-        y = 0  # initial y height
-        for x in xrange(-n, n + 1, s):
-            for z in xrange(-n, n + 1, s):
-                # create a layer of badstone and grass everywhere.
-                self.add_block((x, y - 2, z), GRASS, immediate=False)
-                self.add_block((x, y - 3, z), BADSTONE, immediate=False)
-                if x in (-n, n) or z in (-n, n):
-                    # create outer walls.
-                    for dy in xrange(-2, 3):
-                        self.add_block((x, y + dy, z), BADSTONE, immediate=False)
 
-        # generate the hills randomly
-        o = n - 10
-        for _ in xrange(120):
-            a = random.randint(-o, o)  # x position of the hill
-            b = random.randint(-o, o)  # z position of the hill
-            c = -1  # base of the hill
-            h = random.randint(1, 6)  # height of the hill
-            s = random.randint(4, 8)  # 2 * s is the side length of the hill
-            d = 1  # how quickly to taper off the hills
-            t = random.choice([GRASS, SAND, BRICK])
-            for y in xrange(c, c + h):
-                for x in xrange(a - s, a + s + 1):
-                    for z in xrange(b - s, b + s + 1):
-                        if (x - a) ** 2 + (z - b) ** 2 > (s + 1) ** 2:
-                            continue
-                        if (x - 0) ** 2 + (z - 0) ** 2 < 5 ** 2:
-                            continue
-                        self.add_block((x, y, z), t, immediate=False)
-                s -= d  # decrement side lenth so hills taper off
+        if self.saveModule.hasSaveGame() == True:
+            self.saveModule.loadWorld(self)
+        else:
+            n = 80  # 1/2 width and height of world
+            s = 1  # step size
+            y = 0  # initial y height
+            for x in xrange(-n, n + 1, s):
+                for z in xrange(-n, n + 1, s):
+                    # create a layer stone an grass everywhere.
+                    self.add_block((x, y - 2, z), GRASS, immediate=False)
+                    self.add_block((x, y - 3, z), BADSTONE, immediate=False)
+                    if x in (-n, n) or z in (-n, n):
+                        # create outer walls.
+                        for dy in xrange(-2, 3):
+                            self.add_block((x, y + dy, z), BADSTONE, immediate=False)
+
+            # generate the hills randomly
+            o = n - 10
+            for _ in xrange(120):
+                a = random.randint(-o, o)  # x position of the hill
+                b = random.randint(-o, o)  # z position of the hill
+                c = -1  # base of the hill
+                h = random.randint(1, 6)  # height of the hill
+                s = random.randint(4, 8)  # 2 * s is the side length of the hill
+                d = 1  # how quickly to taper off the hills
+                t = random.choice([GRASS, SNOW, SAND])
+                for y in xrange(c, c + h):
+                    for x in xrange(a - s, a + s + 1):
+                        for z in xrange(b - s, b + s + 1):
+                            if (x - a) ** 2 + (z - b) ** 2 > (s + 1) ** 2:
+                                continue
+                            if (x - 0) ** 2 + (z - 0) ** 2 < 5 ** 2:
+                                continue
+                            self.add_block((x, y, z), t, immediate=False)
+                    s -= d  # decrement side lenth so hills taper off
 
     def hit_test(self, position, vector, max_distance = NODE_SELECTOR):
         """ Line of sight search from current position. If a block is
@@ -834,6 +843,8 @@ class Window(pyglet.window.Window):
             self.toggleGui = not self.toggleGui
         elif symbol == key.F2:
             self.toggleLabel = not self.toggleLabel
+        elif symbol == key.F5:
+            self.model.saveModule.saveWorld(self.model)
         elif symbol == key.F12:
             self.screenshot = pyglet.image.get_buffer_manager().get_color_buffer().save('screenshot.png')
         elif symbol in self.num_keys:
---
+++ saveModule.py
@@ -0,0 +1,92 @@
+'''
+ ________                                        ______                       ______     __
+|        \                                      /      \                     /      \   |  \ 
+ \$$$$$$$$______    ______    ______   ______  |  $$$$$$\  ______   ______  |  $$$$$$\ _| $$_
+   | $$  /      \  /      \  /      \ |      \ | $$   \$$ /      \ |      \ | $$_  \$$|   $$ \ 
+   | $$ |  $$$$$$\|  $$$$$$\|  $$$$$$\ \$$$$$$\| $$      |  $$$$$$\ \$$$$$$\| $$ \     \$$$$$$
+   | $$ | $$    $$| $$   \$$| $$   \$$/      $$| $$   __ | $$   \$$/      $$| $$$$      | $$ __
+   | $$ | $$$$$$$$| $$      | $$     |  $$$$$$$| $$__/  \| $$     |  $$$$$$$| $$        | $$|  \ 
+   | $$  \$$     \| $$      | $$      \$$    $$ \$$    $$| $$      \$$    $$| $$         \$$  $$
+    \$$   \$$$$$$$ \$$       \$$       \$$$$$$$  \$$$$$$  \$$       \$$$$$$$ \$$          \$$$$
+
+
+Copyright (C) 2013 Michael Fogleman
+Copyright (C) 2018 Stefano Peris <xenon77.dev@gmail.com>
+
+Github repository: <https://github.com/XenonCoder/Terracraft>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+'''
+
+
+import main # we need the blocktypes from the main program
+import json
+import os
+from time import gmtime, strftime
+
+class saveModule(object):
+    def __init__(self):
+        # "tarnslate" the block texture tuples into readable words for saving
+        self.coordDictSave = { str(main.DIRT):'DIRT', str(main.GRASS):'GRASS', str(main.SNOW):'SNOW',
+        str(main.SAND):'SAND', str(main.BRICK):'BRICK', str(main.TREE):'TREE', str(main.LEAVES):'LEAVES',
+        str(main.WOODEN_PLANKS):'WOODEN_PLANKS', str(main.BADSTONE):'BADSTONE' }
+        # "tarnslate" the words back into tuples for loading
+        self.coordDictLoad = { 'DIRT':main.DIRT, 'GRASS':main.GRASS, 'SNOW':main.SNOW, 'SAND':main.SAND,
+        'BRICK':main.BRICK, 'TREE':main.TREE, str(main.WOODEN_PLANKS):'WOODEN_PLANKS', 'BADSTONE':main.BADSTONE }
+        
+        self.saveGameFile = 'savegame.sav'
+        
+    def printStuff(self, txt):
+        print((strftime("%d-%m-%Y %H:%M:%S|", gmtime()) + str(txt) ))
+    
+    def hasSaveGame(self):
+        if os.path.exists(self.saveGameFile):
+            return True
+        else:
+            return False
+    
+    def loadWorld(self, model):
+        self.printStuff('start loading...') 
+        fh = open(self.saveGameFile, 'r')
+        worldMod = fh.read()
+        fh.close()
+        
+        worldMod = worldMod.split('\n')
+        
+        for blockLine in worldMod:
+            # remove the last empty line
+            if blockLine != '':
+                coords, blockType = blockLine.split(' => ')
+                # convert the json list into tuple; json ONLY get lists but we need tuples
+                # translate the readable word back into the texture coords
+                model.add_block( tuple(json.loads(coords)), self.coordDictLoad[blockType], False )
+        
+        self.printStuff('loading completed')
+        
+    def saveWorld(self, model):
+        self.printStuff('start saving...')
+        fh = open(self.saveGameFile, 'w')
+        
+        # build a string to save it in one action
+        worldString = ''
+        
+        for block in model.world:
+            # convert the block coords into json
+            # convert with the translation dictionary the block type into a readable word
+            worldString += json.dumps(block) + ' => ' + self.coordDictSave[ str(model.world[block]) ] + '\n'
+
+        fh.write(worldString)
+        fh.close()
+        self.printStuff('saving completed')
