3d7c277a7069c4e5fe0cf1b298fe6d4f7974c7a2
XenonCoder <xenon77.dev@gmail.com>
2018-04-08 01:29:57 +0200

    #18 Module blocks.py added

---
+++ blocks.py
@@ -0,0 +1,77 @@
+'''
+ ________                                        ______                       ______     __
+|        \                                      /      \                     /      \   |  \ 
+ \$$$$$$$$______    ______    ______   ______  |  $$$$$$\  ______   ______  |  $$$$$$\ _| $$_
+   | $$  /      \  /      \  /      \ |      \ | $$   \$$ /      \ |      \ | $$_  \$$|   $$ \ 
+   | $$ |  $$$$$$\|  $$$$$$\|  $$$$$$\ \$$$$$$\| $$      |  $$$$$$\ \$$$$$$\| $$ \     \$$$$$$
+   | $$ | $$    $$| $$   \$$| $$   \$$/      $$| $$   __ | $$   \$$/      $$| $$$$      | $$ __
+   | $$ | $$$$$$$$| $$      | $$     |  $$$$$$$| $$__/  \| $$     |  $$$$$$$| $$        | $$|  \ 
+   | $$  \$$     \| $$      | $$      \$$    $$ \$$    $$| $$      \$$    $$| $$         \$$  $$
+    \$$   \$$$$$$$ \$$       \$$       \$$$$$$$  \$$$$$$  \$$       \$$$$$$$ \$$          \$$$$
+
+
+Copyright (C) 2013 Michael Fogleman
+Copyright (C) 2018 Stefano Peris <xenon77.dev@gmail.com>
+
+Github repository: <https://github.com/XenonCoder/Terracraft>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+TEXTURE_PATH = 'img/textures.png'
+
+def tex_coord(x, y, n=4):
+    """ Return the bounding vertices of the texture square.
+
+    """
+    m = 1.0 / n
+    dx = x * m
+    dy = y * m
+    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
+
+def tex_coords(top, bottom, side):
+    """ Return a list of the texture squares for the top, bottom and side.
+
+    """
+    top = tex_coord(*top)
+    bottom = tex_coord(*bottom)
+    side = tex_coord(*side)
+    result = []
+    result.extend(top)
+    result.extend(bottom)
+    result.extend(side * 4)
+    return result
+
+# top, bottom, side (x, y)
+DIRT = tex_coords((0, 1), (0, 1), (0, 1))
+DIRT_WITH_GRASS = tex_coords((1, 0), (0, 1), (0, 0))
+SAND = tex_coords((1, 1), (1, 1), (1, 1))
+COBBLESTONE = tex_coords((2, 0), (2, 0), (2, 0))
+BRICK_COBBLESTONE = tex_coords((3, 0), (3, 0), (3, 0))
+BRICK = tex_coords((3, 1), (3, 1), (3, 1))
+BADSTONE = tex_coords((2, 1), (2, 1), (2, 1))
+TREE = tex_coords((1, 2), (1, 2), (0, 2))
+LEAVES = tex_coords((2, 2), (2, 2), (2, 2))
+SNOW = tex_coords((1, 3), (0, 1), (0, 3))
+WOODEN_PLANKS = tex_coords((2, 3), (2, 3), (2, 3))
+
+FACES = [
+    ( 0, 1, 0),
+    ( 0,-1, 0),
+    (-1, 0, 0),
+    ( 1, 0, 0),
+    ( 0, 0, 1),
+    ( 0, 0,-1),
+]
--- main.py
+++ main.py
@@ -44,6 +44,7 @@ from pyglet.gl import *
 from pyglet.graphics import TextureGroup
 from pyglet.window import key, mouse
 
+from blocks import *
 import saveLoadModule
 
 
@@ -121,55 +122,6 @@ def cube_vertices(x, y, z, n):
         x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n,  # front
         x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n,  # back
     ]
-
-
-def tex_coord(x, y, n=4):
-    """ Return the bounding vertices of the texture square.
-
-    """
-    m = 1.0 / n
-    dx = x * m
-    dy = y * m
-    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
-
-
-def tex_coords(top, bottom, side):
-    """ Return a list of the texture squares for the top, bottom and side.
-
-    """
-    top = tex_coord(*top)
-    bottom = tex_coord(*bottom)
-    side = tex_coord(*side)
-    result = []
-    result.extend(top)
-    result.extend(bottom)
-    result.extend(side * 4)
-    return result
-
-
-TEXTURE_PATH = 'img/textures.png'
-
-# top, bottom, side (x, y)
-DIRT = tex_coords((0, 1), (0, 1), (0, 1))
-DIRT_WITH_GRASS = tex_coords((1, 0), (0, 1), (0, 0))
-SAND = tex_coords((1, 1), (1, 1), (1, 1))
-COBBLESTONE = tex_coords((2, 0), (2, 0), (2, 0))
-BRICK_COBBLESTONE = tex_coords((3, 0), (3, 0), (3, 0))
-BRICK = tex_coords((3, 1), (3, 1), (3, 1))
-BADSTONE = tex_coords((2, 1), (2, 1), (2, 1))
-TREE = tex_coords((1, 2), (1, 2), (0, 2))
-LEAVES = tex_coords((2, 2), (2, 2), (2, 2))
-SNOW = tex_coords((1, 3), (0, 1), (0, 3))
-WOODEN_PLANKS = tex_coords((2, 3), (2, 3), (2, 3))
-
-FACES = [
-    ( 0, 1, 0),
-    ( 0,-1, 0),
-    (-1, 0, 0),
-    ( 1, 0, 0),
-    ( 0, 0, 1),
-    ( 0, 0,-1),
-]
 
 
 def normalize(position):
